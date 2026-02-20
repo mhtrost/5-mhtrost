c4615b509198a4957fe9c485872aa27c01814436
mhtrost <mhtrost@students.unwsp.edu>
2026-02-20 09:01:52 -0600

    Assignment 5

--- program_2.py
+++ program_2.py
@@ -9,14 +9,11 @@ def addition_of_two_numbers(num1, num2, answer):
         print(f"That is incorrect. The correct answer is: {correct_answer}. Try again.")
 
 num1 = random.randint(1,300)
-
 num2 = random.randint(1,300)
 
 
 print(f"   {num1}")
-
 print(f"+  {num2}")
-
 print("-------")
 
 answer = float(input("Please enter in your answer: "))
