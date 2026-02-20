aa78c68c6311ed24f4d3912779fba597acee3c64
mhtrost <mhtrost@students.unwsp.edu>
2026-02-19 21:23:13 -0600

    Assignment 5

--- program_1.py
+++ program_1.py
@@ -1,10 +1,9 @@
-def kilometer_conversion(kilometers):    
+def kilometer_conversion(kilometers):  
+
     miles = kilometers * 0.6214
+
     return miles   
 
-#### This piece of the code has been done for you,
-#### you only need to worry about the actual kilometer
-#### conversion logic in the kilometer_conversion function
 if __name__ == '__main__':
 
     km = float(input("How many kilometers: "))
--- program_2.py
+++ program_2.py
@@ -6,14 +6,18 @@ def addition_of_two_numbers(num1, num2, answer):
     if answer == correct_answer:
         print("Great Job! That is the correct answer!")
     else:
-        print(f"That is incorrect. The correct answer is: {correct_answer}")
+        print(f"That is incorrect. The correct answer is: {correct_answer}. Try again.")
 
-num1 = random.randint(1,50)
-num2 = random.randint(1,50)
+num1 = random.randint(1,300)
 
-print(f"  {num1}")
-print(f"+ {num2}")
-print("------")
+num2 = random.randint(1,300)
+
+
+print(f"   {num1}")
+
+print(f"+  {num2}")
+
+print("-------")
 
 answer = float(input("Please enter in your answer: "))
 
