f2395fa7cf81e32e8af2e4b967d59c1f920174b6
mhtrost <mhtrost@students.unwsp.edu>
2026-02-24 11:05:20 -0600

    Assignment 6

--- program_1.py
+++ program_1.py
@@ -1,14 +1,8 @@
 def kilometer_conversion(kilometers):  
-
     miles = kilometers * 0.6214
-
     return miles   
 
 if __name__ == '__main__':
-
     km = float(input("How many kilometers: "))
-
     converted_miles = kilometer_conversion(km)
-    
     print(f"That is equal to: {converted_miles:.2f} miles.")
-
